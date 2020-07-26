76b8a7100a2c4c5f56422e36a5d7288c1e06b59a
Prateek Rathore <prateek@Prateeks-MacBook-Pro.local>
2020-07-26 22:02:58 +0530

    add: show random opening name

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -5,30 +5,40 @@ import ChessBoard from '../components';
 const { width } = Dimensions.get('window');
 
 const POSSIBLE_MOVES = [
-  { to: 'g3', from: 'g2' },
-  { to: 'd4', from: 'd2' },
-  { to: 'f3', from: 'g1' },
-  { to: 'c4', from: 'c2' },
-  { to: 'e4', from: 'e2' }
+  { to: 'g3', from: 'g2', name: 'King\'s Fianchetto Opening' },
+  { to: 'd4', from: 'd2', name: 'Queen\'s Pawn Opening' },
+  { to: 'f3', from: 'g1', name: 'Réti Opening' },
+  { to: 'c4', from: 'c2', name: 'English Opening' },
+  { to: 'e4', from: 'e2', name: 'King\'s Pawn Opening' }
 ]
 
 export default class Game extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentOpening: null
+    }
   }
 
   makeRandomMove = () => {
+    const { currentOpening } = this.state;
+    currentOpening && this.resetGame();
+
     const randomIndex = Math.floor(Math.random() * POSSIBLE_MOVES.length);
     const randomMove = POSSIBLE_MOVES[randomIndex];
-
+    
     this.board.movePiece(randomMove.to, randomMove.from);
+    this.setState({ currentOpening :randomMove.name })
   };
 
   resetGame = () => {
+    this.setState({ currentOpening : null})
     this.board.resetBoard();
   }
 
   render() {
+    const { currentOpening } = this.state;
+
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
@@ -38,6 +48,7 @@ export default class Game extends Component {
           ref={board => this.board = board}
           size={width - (width / 6)}
         />
+        {currentOpening && <Text style={styles.openingShow}>{`Currently Showing: ${currentOpening}`  }</Text>}
         <View style={styles.buttonContainer}>
           <Button title={"Reset Board"} onPress={this.resetGame} />
           <Button title={"Show Random Opening"} onPress={this.makeRandomMove} />
@@ -58,6 +69,9 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: '500',
     padding: 20,
+  },
+  openingShow: {
+    margin: 20
   },
   buttonContainer: {
     flexDirection: 'row',
